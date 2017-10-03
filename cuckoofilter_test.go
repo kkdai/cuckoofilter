756b0c6bc55714ab9b63912af96b49ef9f3b284a
Evan Lin <evanslin@gmail.com>
2017-10-04 02:38:03 +0800

    More testing case

--- cuckoofilter_test.go
+++ cuckoofilter_test.go
@@ -3,7 +3,7 @@ package cuckoofilter
 import "testing"
 
 func TestInsert(t *testing.T) {
-	ck := NewCukooFilter(1000)
+	ck := NewCuckooFilter(1000)
 	if ret := ck.Insert([]byte("abc")); !ret {
 		t.Error("Insert1: failed")
 	}
@@ -11,13 +11,19 @@ func TestInsert(t *testing.T) {
 		t.Error("Insert2: failed")
 	}
 
+	if ret := ck.Delete([]byte("bcd")); !ret {
+		t.Error("Delete: failed")
+	}
+
+	if ret := ck.Lookup([]byte("bcd")); !ret {
+		t.Error("Lookup: failed")
+	}
+
 	if ret := ck.Insert([]byte("bcd")); !ret {
 		t.Error("Insert2: failed")
 	}
-	if ret := ck.Insert([]byte("bcd")); !ret {
-		t.Error("Insert2: failed")
-	}
-	if count := ck.Count(); count != 4 {
+
+	if count := ck.Count(); count != 3 {
 		t.Error("Couting errer:", count)
 	}
 }
